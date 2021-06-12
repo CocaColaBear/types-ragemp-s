e65213d92a5810f36b987ac90977b9a58127a5f1
Guy Incognito <lJohhnyl@outlook.com>
2021-06-12 22:18:33 +0300

    v2: Move markers enum in own definition file

--- enums.d.ts
+++ enums.d.ts
@@ -2,45 +2,11 @@
 /// <reference path="enums/colshape.d.ts" />
 /// <reference path="enums/entity.d.ts" />
 /// <reference path="enums/event.d.ts" />
+/// <reference path="enums/markers.d.ts" />
 /// <reference path="enums/ped.d.ts" />
 /// <reference path="enums/vehicle.d.ts" />
 
 declare namespace RageEnums {
-	const enum Marker {
-		UPSIDE_DOWN_CONE = 0,
-    VERTICAL_CYLINDER = 1,
-    THICK_CEVRON_UP = 2,
-    THIN_CEVRON_UP = 3,
-    CHECKERED_FLAG_RECT = 4,
-    CHECKERED_FLAG_CIRCLE = 5,
-    VERTICAL_CIRCLE = 6,
-    PLANE_MODEL = 7,
-    LOST_MC_DARK = 8,
-    LOST_MC_LIGHT = 9,
-    NUMBER_0 = 10,
-    NUMBER_1 = 11,
-    NUMBER_2 = 12,
-    NUMBER_3 = 13,
-    NUMBER_4 = 14,
-    NUMBER_5 = 15,
-    NUMBER_6 = 16,
-    NUMBER_7 = 17,
-    NUMBER_8 = 18,
-    NUMBER_9 = 19,
-    CHEVRON_UP_1 = 20,
-    CHEVRON_UP_2 = 21,
-    CHEVRON_UP_3 = 22,
-    HORIZONTAL_CIRCLE_FLAT = 23,
-    REPLAY_ICON = 24,
-    HORIZONTAL_CIRCLE_SKINNY = 25,
-    HORIZONTAL_CIRCLE_ARROW = 26,
-    HORIZONTAL_SPLIT_ARROW_CIRCLE = 27,
-    DEBUG_SPHERE = 28,
-    DOLLOR_SIGN = 29,
-    HORIZONTAL_BARS = 30,
-    WOLF_HEAD = 31
-	}
-
 	const enum PlayerProp {
 		HELMET = 0,
 		GLASSES = 1,

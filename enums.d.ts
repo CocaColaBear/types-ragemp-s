143191c0c3bf327664976a5e980f63485cc66c47
Guy Incognito <lJohhnyl@outlook.com>
2021-06-12 22:22:40 +0300

    v2: Move weather into own definition file

--- enums.d.ts
+++ enums.d.ts
@@ -5,20 +5,4 @@
 /// <reference path="enums/markers.d.ts" />
 /// <reference path="enums/ped.d.ts" />
 /// <reference path="enums/vehicle.d.ts" />
-
-declare namespace RageEnums {
-	const enum Weather {
-		BLIZZARD = "BLIZZARD",
-		CLEAR = "CLEAR",
-		CLEARING = "CLEARING",
-		CLOUDS = "CLOUDS",
-		EXTRA_SUNNY = "EXTRASUNNY",
-		FOGGY = "FOGGY",
-		OVERCAST = "OVERCAST",
-		RAIN = "RAIN",
-		SMOG = "SMOG",
-		SNOW_LIGHT = "SNOWLIGHT",
-		THUNDER = "THUNDER",
-		XMAS = "XMAS"
-	}
-}
+/// <reference path="enums/weather.d.ts" />
